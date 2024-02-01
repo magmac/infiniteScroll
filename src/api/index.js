1d940bb0a465b8eb549d50f75d8edd630a6b099e
chi <cky1857@gmail.com>
2024-02-01 23:37:33 +0800

    page

--- src/api/index.js
+++
@@ -1,3 +0,0 @@
-import { get } from './http'
-
-export const getReop = params => get('/users/doggy8088/repos', params)
--- src/stores/mutation-types.js
+++
@@ -1,3 +0,0 @@
-export const REPOS = 'REPOS'
-export const LOADING = 'LOADING'
-export const END = 'END'
